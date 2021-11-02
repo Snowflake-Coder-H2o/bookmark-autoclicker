2e4b2abf2b79ae4202e58f7ec17b9b0c7947edee
Silasssssss <92600501+Silasssssss@users.noreply.github.com>
2021-11-02 13:58:11 -0500

    Inspector V1.01
    Fix conflicting variable names

--- autoclicker.js
+++ autoclicker.js
@@ -1,7 +1,7 @@
 // im kool
 var acurrentElement = document.body;
 var a_enabled = false
-var aenableKey = "["
+var aEnableKey = "["
 
 alert('AutoClicker activated! Use "[" to toggle it!\n\nChange the keybind with "\\".\n\n~ SilasDevs 2021 ~')
 
@@ -10,16 +10,10 @@ document.addEventListener('mouseover', function (e) {
 });
 
 window.addEventListener('keydown', function (e) {
-    if (e.key == aenableKey) {
+    if (e.key == aEnableKey) {
         a_enabled = !a_enabled
-
-        if (i_enabled) {
-            alert("Activated auto-clicker!")
-        } else {
-            alert("Disabled auto-clicker!")
-        }
     } else if (e.key == "\\") {
-         aenableKey = prompt("Change keybind:")[0].toLowerCase()   
+         aEnableKey = prompt("Change keybind:")[0].toLowerCase()   
     }
 });
 
