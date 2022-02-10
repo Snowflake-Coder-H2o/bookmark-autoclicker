3a4220240a97b1c68db40a4f3d98b77f195d54d2
Silas Campbell <92600501+Silasssssss@users.noreply.github.com>
2022-02-10 11:40:44 -0600

    Added alert system && made intervals cooler.

--- autoclicker.js
+++ autoclicker.js
@@ -3,22 +3,70 @@ var acurrentElement = document.body;
 var a_enabled = false
 var aEnableKey = "["
 
-alert('AutoClicker activated! Use "[" to toggle it!\n\nChange the keybind with "\\".\n\n~ SilasDevs 2021 ~')
+var css = `
+.autoclicker-alert {
+  display: flex;
+  position: fixed;
+  top: 10px;
+  left: 10px;
+  background-color: #404040;
+  align-items: center;
+  padding: 7px 8px 7px 5px;
+  border-left: solid red;
+  border-radius: 3px;
+  opacity: 1;
+  z-index: 99999999999;
+  transition: opacity 200ms;
+}
+.autoclicker-alert span {
+  text-align: center;
+  color: white;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`
+
+function sendAlert(text, border) {
+  var style = document.createElement('style');
+  document.head.appendChild(style)
+  style.type = 'text/css';
+  style.appendChild(document.createTextNode(css));
+
+  var alert = document.createElement('div')
+  alert.style = "border-left: solid " + border
+  var span = document.createElement('span')
+  alert.classList.add('autoclicker-alert')
+  span.innerText = text
+  alert.appendChild(span)
+
+  document.body.appendChild(alert)
+  
+  setTimeout(function() {alert.style.opacity = 0}, 3000)
+  setTimeout(function() {alert.remove()}, 3200)
+}
+
+sendAlert("🖱️ Autoclicker loaded!\n Use the [ key to enable!", "lime")
 
 document.addEventListener('mouseover', function (e) {
     acurrentElement = e.target;
 });
 
+var interval = setInterval(function(){}) // make global interval, only temporarily used
+
 window.addEventListener('keydown', function (e) {
     if (e.key == aEnableKey) {
         a_enabled = !a_enabled
+        if (a_enabled) {
+            sendAlert("✅ Started clicking!", "lime")
+            interval = setInterval(function() {
+                acurrentElement.click()
+            }, 0)
+        } else {
+            sendAlert("❌ Stopped clicking!", "red")
+            clearInterval(interval)
+        }
     } else if (e.key == "\\") {
          aEnableKey = prompt("Change keybind:")[0].toLowerCase()   
     }
 });
 
-setInterval(function() {
-  if (a_enabled) {
-    acurrentElement.click()
-  }
-}, 1)
+
